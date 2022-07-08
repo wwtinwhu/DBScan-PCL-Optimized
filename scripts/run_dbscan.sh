3a2e6956ed68aa15d2ae1b972751f3802a197053
Daniel Tobon <danieltobonco@gmail.com>
2022-07-08 05:57:56 -0500

    moved shell scripts to folder

---
+++ scripts/run_dbscan.sh
@@ -0,0 +1,11 @@
+# xhost -local:root
+# Allow X server connection
+xhost +local:root
+docker run -it --rm \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume=`pwd`:/tmp \
+    ghcr.io/danieltobon43/dbscan-octrees:latest
+# Disallow X server connection
+xhost -local:root
